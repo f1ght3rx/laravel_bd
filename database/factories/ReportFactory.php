18eaae9430d3e482ccb1bc9f8c3d898a2aaa1448
roman v <dj_dominator1337@mail.ru>
2026-04-15 15:15:13 +0500

    nachalo_16_zadanie5

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Hamcrest\Description;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ReportFactory extends Factory
+{
+    public function definition(): array
+    {
+        $faker = \Faker\Factory::create();
+        return [
+            'number' => $faker->numerify('aaa-###'),
+            'desciption'=> $faker->paragraph,
+            'created_at' => $faker->dateTimeBetween('-1 Week','now'),
+            'status_id' => 1
+        ];
+    }
+}
